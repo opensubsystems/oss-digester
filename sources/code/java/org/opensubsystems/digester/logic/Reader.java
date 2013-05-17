02b26ab9d0b60e156f270d77b2cf85a917c37a9b
bastafidli <bastafidli@opensubsystems.org>
2013-05-17 15:08:06 -0400

    Refactored to correct package.

---
+++ sources/code/java/org/opensubsystems/digester/logic/Reader.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright (C) 2003 - 2013 OpenSubsystems.com/net/org and its owners. All rights reserved.
+ * 
+ * This file is part of OpenSubsystems.
+ *
+ * OpenSubsystems is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+ */
+
+package org.opensubsystems.digester.logic;
+
+import org.opensubsystems.digester.data.DigesterContext;
+import org.opensubsystems.core.error.OSSException;
+import org.opensubsystems.core.logic.StatelessController;
+
+/**
+ * Interface representing reader capable of reading content from a source and 
+ * making it available in the form of records.
+ *
+ * @author bastafidli
+ */
+public interface Reader<S, C extends DigesterContext, R> extends StatelessController
+{
+   /**
+    * Open the source for reading.
+    * 
+    * @param context - context within which is the content being read.
+    * @param source - source from this the reader should read data
+    * @throws OSSException - an error has occurred 
+    */
+   void open(
+      C context,
+      S source
+   ) throws OSSException;
+   
+   /**
+    * Read record from the source initialized during the open call.
+    *
+    * @param context - context within which is the content being read.
+    * @return R - record read by the reader or null if there are no more data to
+    *             read.
+    * @throws OSSException - an error has occurred 
+    */
+   R read(
+      C context
+   ) throws OSSException;
+
+   /**
+    * Close the context for reading and cleanup any resources allocated in open.
+    * This operation must be called once open is called and succeeded.
+    * 
+    * @param context - context returned from open
+    */
+   void close(
+      C context
+   ) throws OSSException;
+}
